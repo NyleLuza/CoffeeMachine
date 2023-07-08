a22a9d75409b21f3c9ae731eb72c8338e6db4200
NyleLuza <Jluza21@gmail.com>
2023-07-08 00:17:49 -0700

    finished project

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -25,40 +25,84 @@ MENU = {
 }
 
 profit = 0
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
-#TODO 1: Create loop to ask for input and to turn off the coffee machine
 
-cof = input("what would you like (espresso/latte/cappuccino): ")
+# TODO 1: Create loop to ask for input and to turn off the coffee machine
+
 
 def checkCoffee(m, coffee):
     if coffee in m.keys():
         return coffee
     elif coffee == "report":
-        print(str(resources))
+        print("Water:", resources["water"], "ml")
+        print("Milk:", resources["milk"], "ml")
+        print("Coffee:", resources["coffee"], "g")
+        print("Money: $", profit)
+        return
+    elif coffee == "off":
+        print("GoodBye!")
         return
     else:
-        print("not in dic")
+        print("We do not have this item!")
         return
 
-def checkResources(bev):
-    if(bev == "espresso"):
+
+def resource():
+    if resources["water"] < 0 or resources["milk"] < 0 or resources["coffee"] < 0:
+        if resources["water"] < 0:
+            print("Sorry there isn't enough water!")
+            return False
+        elif resources["milk"] < 0:
+            print("Sorry there isn't enough milk!")
+            return False
+        elif resources["coffee"] < 0:
+            print("Sorry there isn't enough coffee")
+            return False
+        else:
+            return True
+
+
+def checkResources(bev, prof):
+    if bev == "espresso":
         resources["water"] = resources["water"] - MENU["espresso"]["ingredients"]["water"]
         resources["coffee"] = resources["coffee"] - MENU["espresso"]["ingredients"]["coffee"]
+        if resource() == False:
+            resources["water"] = resources["water"] + MENU["espresso"]["ingredients"]["water"]
+            resources["coffee"] = resources["coffee"] + MENU["espresso"]["ingredients"]["coffee"]
+            return 0
+        prof = MENU["espresso"]["cost"]
+        return prof
 
-    if(bev == "latte"):
+    if bev == "latte":
         resources["water"] = resources["water"] - MENU["latte"]["ingredients"]["water"]
         resources["coffee"] = resources["coffee"] - MENU["latte"]["ingredients"]["coffee"]
         resources["milk"] = resources["milk"] - MENU["latte"]["ingredients"]["milk"]
+        if resource() == False:
+            resources["water"] = resources["water"] + MENU["latte"]["ingredients"]["water"]
+            resources["coffee"] = resources["coffee"] + MENU["latte"]["ingredients"]["coffee"]
+            resources["milk"] = resources["milk"] + MENU["latte"]["ingredients"]["milk"]
+            return 0
+        prof = MENU["latte"]["cost"]
+        return prof
 
-    if (bev == "cappuccino"):
+    if bev == "cappuccino":
         resources["water"] = resources["water"] - MENU["cappuccino"]["ingredients"]["water"]
         resources["coffee"] = resources["coffee"] - MENU["cappuccino"]["ingredients"]["coffee"]
         resources["milk"] = resources["milk"] - MENU["cappuccino"]["ingredients"]["milk"]
+        if resource() == False:
+            resources["water"] = resources["water"] + MENU["cappuccino"]["ingredients"]["water"]
+            resources["coffee"] = resources["coffee"] + MENU["cappuccino"]["ingredients"]["coffee"]
+            resources["milk"] = resources["milk"] + MENU["cappuccino"]["ingredients"]["milk"]
+            return 0
+        prof = MENU["latte"]["cost"]
+        return prof
+
 
 def coins(choice):
     quarters = (float)(input("How Many Quarters?: "))
@@ -69,16 +113,24 @@ def coins(choice):
     nickelsT = nickels * 0.05
     pennies = (float)(input("How Many Pennies?: "))
     penniesT = pennies * 0.01
-
     total = quartersT + dimesT + nickelsT + penniesT
-    if(total<MENU[choice]["cost"]):
+    if (total < MENU[choice]["cost"]):
         print("You do not have enough coins")
 
     return total
-while cof!= "off":
-    choice = checkCoffee(MENU, cof)
-    print("Please Insert Coins.")
-    total = coins(choice)
-    print(total)
-    cof = input("what would you like (espresso/latte/cappuccino): ")
 
+
+cof = "on"
+while cof != "off":
+    cof = input("what would you like (espresso/latte/cappuccino): ")
+    choice = checkCoffee(MENU, cof)
+    if choice != "espresso" and choice != "latte" and choice != "cappuccino":
+        continue
+    profitCall = checkResources(choice, profit)
+    if profitCall == 0:
+        continue
+    profit += profitCall
+    print("Please Insert Coins.")
+    change = coins(choice) - MENU[choice]["cost"]
+    print("Here is $",format(change,".2f"),"in change")
+    print("Here is your", cof, "Enjoy!")
